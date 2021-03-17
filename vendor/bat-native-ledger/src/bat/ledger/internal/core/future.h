369d571fd0d5c456ce9940134632e5129f97600b
zenparsing <zenparsing@gmail.com>
2021-03-17 11:14:00 -0400

    Implement changes to bat-native-ledger core classes based on feedback
    - Pass ownership of async value to unique listener
    - Add template overload of AsyncResult::Then that maps the result value
    - Use RefCountedThreadSafe instead of std::shared_ptr
    - Rename Task to Job
    - Use map instead of unordered_map and void* as job map key
    - Derive BATLedgerJob from SupportsWeakPtr
    - Disallow access to BATLedgerContext during construction and destruction
    - Add ContinueWith helper to BATLedgerJob
    - Return references instead of pointers
    - Rename BATLedgerContext::Component to BATLedgerContext::Object
    - Rename AsyncResult to Future

---
+++ vendor/bat-native-ledger/src/bat/ledger/internal/core/future.h
@@ -0,0 +1,153 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef BRAVE_VENDOR_BAT_NATIVE_LEDGER_SRC_BAT_LEDGER_INTERNAL_CORE_FUTURE_H_
+#define BRAVE_VENDOR_BAT_NATIVE_LEDGER_SRC_BAT_LEDGER_INTERNAL_CORE_FUTURE_H_
+
+#include <list>
+#include <type_traits>
+#include <utility>
+
+#include "base/callback.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_refptr.h"
+#include "base/threading/sequenced_task_runner_handle.h"
+#include "third_party/abseil-cpp/absl/types/optional.h"
+
+namespace ledger {
+
+// Represents the result of an asynchronous operation.
+//
+// Example:
+//   Future<int>::Resolver resolver;
+//   resolver.Complete(42);
+//
+//   Future<int> result = resolver.result();
+//   result.Then(base::BindOnce([](int value) {}));
+//
+// Listeners are called on the current SequencedTaskRunner, and are guaranteed
+// to be called asynchronously. Future and Resolver objects are internally
+// reference counted and can be passed between sequences; the internal data
+// structures are updated on the sequence that created the Resolver.
+template <typename T>
+class Future {
+  static_assert(!std::is_reference<T>::value && !std::is_pointer<T>::value,
+                "Future<T> is not supported for pointer or reference types");
+
+ public:
+  using CompleteType = T;
+  using CompleteCallback = base::OnceCallback<void(T)>;
+
+  template <typename U>
+  using MapCompleteCallback = base::OnceCallback<U(T)>;
+
+  void Then(CompleteCallback on_complete) {
+    DCHECK(store_);
+
+    Listener listener = {.on_complete = std::move(on_complete),
+                         .task_runner = base::SequencedTaskRunnerHandle::Get()};
+
+    task_runner_->PostTask(FROM_HERE, base::BindOnce(AddListenerInTask, store_,
+                                                     std::move(listener)));
+
+    store_.reset();
+  }
+
+  template <typename U>
+  Future<U> Then(MapCompleteCallback<U> map_complete) {
+    typename Future<U>::Resolver resolver;
+    Then(base::BindOnce(ThenCompleteCallback<U>, resolver,
+                        std::move(map_complete)));
+    return resolver.future();
+  }
+
+  class Resolver {
+   public:
+    Resolver() {}
+    void Complete(T value) { future_.Complete(std::move(value)); }
+    Future future() const { return future_; }
+
+   private:
+    Future future_;
+  };
+
+ private:
+  Future()
+      : store_(new Store()),
+        task_runner_(base::SequencedTaskRunnerHandle::Get()) {}
+
+  enum class State { kPending, kComplete, kEmpty };
+
+  struct Listener {
+    CompleteCallback on_complete;
+    scoped_refptr<base::SequencedTaskRunner> task_runner;
+  };
+
+  struct Store : public base::RefCountedThreadSafe<Store> {
+    Store() {}
+    State state = State::kPending;
+    absl::optional<T> value;
+    absl::optional<Listener> listener;
+  };
+
+  void Complete(T&& value) {
+    task_runner_->PostTask(
+        FROM_HERE, base::BindOnce(SetCompleteInTask, store_, std::move(value)));
+  }
+
+  template <typename U>
+  static void ThenCompleteCallback(typename Future<U>::Resolver resolver,
+                                   MapCompleteCallback<U> map_complete,
+                                   T value) {
+    resolver.Complete(std::move(map_complete).Run(std::move(value)));
+  }
+
+  static void AddListenerInTask(scoped_refptr<Store> store, Listener listener) {
+    switch (store->state) {
+      case State::kComplete:
+        store->state = State::kEmpty;
+        DCHECK(store->value);
+        listener.task_runner->PostTask(
+            FROM_HERE,
+            base::BindOnce(RunCompleteCallback, std::move(*store->value),
+                           std::move(listener.on_complete)));
+        break;
+      case State::kPending:
+        store->listener = std::move(listener);
+        break;
+      case State::kEmpty:
+        NOTREACHED();
+        break;
+    }
+  }
+
+  static void SetCompleteInTask(scoped_refptr<Store> store, T value) {
+    if (store->state != State::kPending)
+      return;
+
+    store->state = State::kComplete;
+    store->value = std::move(value);
+
+    if (store->listener) {
+      store->state = State::kEmpty;
+      Listener listener = std::move(*store->listener);
+      listener.task_runner->PostTask(
+          FROM_HERE,
+          base::BindOnce(RunCompleteCallback, std::move(*store->value),
+                         std::move(listener.on_complete)));
+    }
+  }
+
+  static void RunCompleteCallback(T value, CompleteCallback on_complete) {
+    std::move(on_complete).Run(std::move(value));
+  }
+
+  scoped_refptr<Store> store_;
+  scoped_refptr<base::SequencedTaskRunner> task_runner_;
+};
+
+}  // namespace ledger
+
+#endif  // BRAVE_VENDOR_BAT_NATIVE_LEDGER_SRC_BAT_LEDGER_INTERNAL_CORE_FUTURE_H_
---
+++ vendor/bat-native-ledger/src/bat/ledger/internal/core/future_unittest.cc
@@ -0,0 +1,53 @@
+/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "bat/ledger/internal/core/future.h"
+#include "base/test/bind.h"
+#include "base/test/task_environment.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace ledger {
+
+class FutureTest : public testing::Test {
+ protected:
+  base::test::TaskEnvironment task_environment_;
+};
+
+TEST_F(FutureTest, CompleteResultSentInFutureTurn) {
+  Future<int>::Resolver resolver;
+  resolver.Complete(10);
+  int value = 0;
+  resolver.future().Then(
+      base::BindLambdaForTesting([&value](int v) { value = v; }));
+  ASSERT_EQ(value, 0);
+  task_environment_.RunUntilIdle();
+  ASSERT_EQ(value, 10);
+}
+
+TEST_F(FutureTest, CompleteCallbacksExecutedInFutureTurn) {
+  Future<int>::Resolver resolver;
+  int value = 0;
+  resolver.future().Then(
+      base::BindLambdaForTesting([&value](int v) { value = v; }));
+  resolver.Complete(1);
+  ASSERT_EQ(value, 0);
+  task_environment_.RunUntilIdle();
+  ASSERT_EQ(value, 1);
+}
+
+TEST_F(FutureTest, ThenMapping) {
+  Future<int>::Resolver resolver;
+  double value = 0;
+  resolver.future()
+      .Then(base::BindOnce([](int v) { return static_cast<double>(v) / 2; }))
+      .Then(base::BindLambdaForTesting([&value](double v) { value = v; }));
+
+  resolver.Complete(1);
+  ASSERT_EQ(value, 0);
+  task_environment_.RunUntilIdle();
+  ASSERT_EQ(value, 0.5);
+}
+
+}  // namespace ledger
