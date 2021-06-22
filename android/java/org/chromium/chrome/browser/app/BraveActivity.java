3fd9e25a08bf0370dc5b201451daa915aca380f4
Deep <deeppandya91@gmail.com>
2021-06-22 06:27:49 -0400

    Update existing implementation

--- android/java/org/chromium/chrome/browser/app/BraveActivity.java
+++ android/java/org/chromium/chrome/browser/app/BraveActivity.java
@@ -452,13 +452,13 @@ public abstract class BraveActivity<C extends ChromeActivityComponent>
             connectivityManager.registerNetworkCallback(networkRequest, mNetworkCallback);
 
             if ((SharedPreferencesManager.getInstance().readInt(
-                     BravePreferenceKeys.BRAVE_APP_OPEN_COUNT)
-                            == 1
-                    && !PackageUtils.isFirstInstall(this))
-                || (SharedPreferencesManager.getInstance().readInt(
-                            BravePreferenceKeys.BRAVE_APP_OPEN_COUNT)
-                                == 7
-                        && PackageUtils.isFirstInstall(this))) {
+                         BravePreferenceKeys.BRAVE_APP_OPEN_COUNT)
+                                == 1
+                        && !PackageUtils.isFirstInstall(this))
+                    || (SharedPreferencesManager.getInstance().readInt(
+                                BravePreferenceKeys.BRAVE_APP_OPEN_COUNT)
+                                    == 7
+                            && PackageUtils.isFirstInstall(this))) {
                 showVpnCalloutDialog();
             }
         }
@@ -507,7 +507,8 @@ public abstract class BraveActivity<C extends ChromeActivityComponent>
                 try {
                     vpnManager.startProvisionedVpnProfile();
                 } catch (SecurityException securityException) {
-                    Ikev2VpnProfile ikev2VpnProfile = BraveVpnUtils.getVpnProfile(BraveActivity.this);
+                    Ikev2VpnProfile ikev2VpnProfile =
+                            BraveVpnUtils.getVpnProfile(BraveActivity.this);
                     Intent intent = vpnManager.provisionVpnProfile(ikev2VpnProfile);
                     startActivityForResult(intent, BRAVE_VPN_PROFILE_REQUEST_CODE);
                 }
@@ -834,7 +835,8 @@ public abstract class BraveActivity<C extends ChromeActivityComponent>
             if (! TextUtils.isEmpty(open_url)) {
                 openNewOrSelectExistingTab(open_url);
             }
-        } else if (resultCode == RESULT_OK && requestCode == BRAVE_VPN_PROFILE_REQUEST_CODE && BraveVpnUtils.isBraveVpnFeatureEnable()) {
+        } else if (resultCode == RESULT_OK && requestCode == BRAVE_VPN_PROFILE_REQUEST_CODE
+                && BraveVpnUtils.isBraveVpnFeatureEnable()) {
             vpnManager.startProvisionedVpnProfile();
         }
         super.onActivityResult(requestCode, resultCode, data);
